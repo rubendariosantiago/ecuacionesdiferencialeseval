3ee9de1e771be83de3dcace3f4b8431c6cac4805
rubendariosantiago <ruben.dario@itesm.mx>
2025-04-18 15:21:24 -0600

    Update config1.js

--- js/config/config1.js
+++ js/config/config1.js
@@ -12,11 +12,11 @@ export default {
   questionMix: {
     theory: {
       count: 2,
-      tags: ["orden", "linealidad"]
+      tags: ["factor-integrante", "lineal-primer-orden"]
     },
     practical: {
       count: 3,
-      types: ["lineales-homogeneas"],
+      types: ["lineales-primer-orden"],
       difficulty: "medium"
     }
   },
